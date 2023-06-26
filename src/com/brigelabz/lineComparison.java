083217430c571047e0b37cc8474d01bd2d4f0195
Supriya Bhurle <supriyabhurle28@gmail.com>
2023-06-26 20:18:19 +0530

    Added : Welcome message

--- src/com/brigelabz/lineComparison.java
+++ src/com/brigelabz/lineComparison.java
@@ -1,2 +1,7 @@
-package com.brigelabz;public class lineComparison {
+package com.brigelabz;
+
+public class lineComparison {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Line Comparison Problem");
+    }
 }
